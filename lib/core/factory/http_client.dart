18787549248a9aca5cd2a568b94b09d54940522c
johnnyfa <johnnyfagundes9@gmail.com>
2026-04-14 22:18:16 -0300

    chore: fix typo in constants directory name
    Renamed `lib/core/constans/` to `lib/core/constants/` and updated the corresponding import in `http_client.dart`.

--- lib/core/factory/http_client.dart
+++ lib/core/factory/http_client.dart
@@ -1,4 +1,4 @@
-import 'package:finances_control/core/constans/app_constants.dart';
+import 'package:finances_control/core/constants/app_constants.dart';
 import 'package:finances_control/core/http/dio_get_client.dart';
 import 'package:finances_control/core/http/dio_impl.dart';
 import 'package:dio/dio.dart';
